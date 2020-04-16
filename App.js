eecdf66a2b8b42638764c33171503a95d7272b99
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-04-16 22:01:29 +0530

    add useState hook

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, FlatList} from 'react-native';
 import Header from './components/header';
+import {uuid} from 'uuid4';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'},
+  ]);
   return (
     <View style={styles.container}>
       {/* <Text style={styles.text}>Hello WOrld</Text> */}
       <Header title="Shopping List" />
+      <FlatList />
       {/* <Image
         source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}}
         style={styles.img}
