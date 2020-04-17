60b654f1249577a20f04be185adf63cf899153ea
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-04-17 22:33:58 +0530

    add some styling to ListItem.js

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 const ListItem = props => {
   return (
-    <View style={styles.header}>
-      <Text style={styles.text}>{props.title}</Text>
-    </View>
+    <TouchableOpacity style={styles.listItem}>
+      <View style={styles.listItemView}>
+        <Text style={styles.listItemText}>{props.item.text}</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  
+  listItem: {
+    padding: 15,
+    backgroundColor: '#f8f8f8',
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+  },
+  listItemView: {
+    flexDirection: 'row',
+    justifyCOntent: 'sapace-between',
+    alignItems: 'center',
+  },
+  listItemText:{
+    fontSize: 18,
+  },
 });
 
 export default ListItem;
